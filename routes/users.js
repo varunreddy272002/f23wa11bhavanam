1764f5b2bda113aa03607a6583cdb02574c2aec3
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-10-25 15:47:41 -0500

    done with bonus

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,21 @@ var express = require('express');
 var router = express.Router();
 let sum = 0;
 let more = 0;
+
+router.get('/computation', function(req, res, next){
+
+  var x = req.x;
+
+  if(!x){
+
+    x = Math.random()*1000;
+
+  }
+
+  res.send(`Math.log(${x}) is: ${Math.log(x)}`);
+
+});
+
 router.get('/', function(req, res, next) {
 
   more++; // Increment 'more' by 1
